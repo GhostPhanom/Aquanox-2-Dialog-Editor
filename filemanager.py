a6a82affcbee107d334900503e509fe6e39f6e02
GhostPhanom <theghostphanom@gmail.com>
2024-05-26 12:08:22 +0200

    Update filemanager.py

--- filemanager.py
+++ filemanager.py
@@ -6,6 +6,206 @@ aquanox_basepath = "C:/Games/AquaNox 2 Revelation/Aquanox-2-Dialog-Editor"#Atten
 locale = "de"
 export_basepath = "C:/Games/AquaNox 2 Revelation/Aquanox-2-Dialog-Editor/export"
 
+class Stake_List:
+	filepath_des = pathlib.Path(aquanox_basepath, "dat/sty/stake.des")
+	filepath_loc = pathlib.Path(aquanox_basepath, "dat/sty/", locale, "stake.loc")
+	table = {}
+	last_key = -1
+
+	def Export(self):
+		currenttimepath = time.strftime("%Y-%m-%d-%H-%M")
+		outputpath_des = pathlib.Path(export_basepath, currenttimepath, "dat/sty/stake.des")
+		outputpath_loc = pathlib.Path(export_basepath, currenttimepath, "dat/sty/", locale, "stake.loc")
+
+		outputpath_des.parent.mkdir(exist_ok=True, parents=True)
+
+		output_des = "[Table]\n"
+		output_des += "{\n"
+		output_des += "\n"
+
+		for entry in self.table.keys():
+			char = self.table[entry]
+			output_des += f"    [{entry}]\n"
+			output_des += "    {\n"
+			if char.Comment_des != "NO .DES COMMENT":
+				output_des += f"        //{char.Comment_des}\n"
+			output_des += f'        Key = {char.Key}\n'
+			output_des += f'        Dialog = {char.Dialog}\n'
+			output_des += f'        Person = {char.Person}\n'
+			output_des += f'        Wav = "{char.Wav}"\n'
+			output_des += f'        Mood = {char.Mood}\n'
+			output_des += "    }\n"
+			output_des += "\n"
+		output_des += "}\n"
+		#print(output_des)
+		outputpath_des.write_text(output_des, encoding = 'cp1252')
+
+		outputpath_loc.parent.mkdir(exist_ok=True, parents=True)
+
+		output_loc = "[Table]\n"
+		output_loc += "{\n"
+		output_loc += "\n"
+
+		for entry in self.table.keys():
+			char = self.table[entry]
+			output_loc += f"    [{entry}]\n"
+			output_loc += "    {\n"
+			if char.Comment_loc != "NO .LOC COMMENT":
+				output_loc += f"        //{char.Comment_des}\n"
+			output_loc += f'        Key = {char.Key}\n'
+			output_loc += f'        Text = "{char.Text}"\n'
+			output_loc += "    }\n"
+			output_loc += "\n"
+		output_loc += "}\n"
+		#print(output_loc)
+		outputpath_loc.write_text(output_loc, encoding = 'cp1252')
+
+
+class Stake:
+	Key = -1
+	Dialog = -1
+	Person = -1
+	Wav = "-1"
+	WavPath = "-1"
+	Mood = -1
+	Comment_des = "NO .DES COMMENT"
+	Comment_loc = "NO .LOC COMMENT"
+
+	def Print(self):
+		print(f"Key: {self.Key}")
+		print(f"Dialog: {self.Dialog}")
+		print(f"Person: {self.Person}")
+		print(f"Wav: {self.Wav}")
+		print(f"WavPath: {self.WavPath}")
+		print(f"Mood: {self.Mood}")
+		print(f"Comment_des: {self.Comment_des}")
+		print(f"Comment_loc: {self.Comment_loc}")
+
+def ParseStakes(listobj):
+	if hasattr(listobj, "filepath_des") and hasattr(listobj, "filepath_loc"):
+		file_des = open(listobj.filepath_des, 'r')
+		file_loc = open(listobj.filepath_loc, 'r')
+
+	Lines = file_des.readlines()
+	readstate = "TableStart"
+	tempchar = Stake()
+	entryname = ""
+	for line in Lines:
+		line = line.lstrip()
+		line = line.rstrip()
+		if len(line) == 0: #Empty Line
+			continue
+		if readstate == "TableStart":#Search for [Table]
+			if "[Table]" in line:
+				readstate = "TableOpen"
+				continue
+		if readstate == "TableOpen":#Search for { after [Table]
+			if line == "{":
+				readstate = "SearchTableEntry"
+				continue
+		if readstate == "SearchTableEntry":#Search for [
+			if line[0] == "[":
+				line = line.split("[")[1]
+				line = line.split("]")[0]
+				entryname = line
+				readstate = "TableEntryOpen"
+				continue
+		if readstate == "SearchTableEntry":#Search for last }
+			if line[0] == "}":
+				readstate = "TableClosed"
+				break#End
+		if readstate == "TableEntryOpen":
+			if line == "{":
+				readstate = "TableEntryData"
+				continue
+		if readstate == "TableEntryData":
+			if "//" in line:
+				tempchar.Comment_des = line.split("/")[2]
+			if "Key = " in line:
+				tempchar.Key = line.split(" ")[2]
+			if "Dialog = " in line:
+				tempchar.Dialog = line.split(" ")[2]
+			if "Person = " in line:
+				tempchar.Person = line.split(" ")[2]
+			if "Wav = " in line:
+				tempchar.Wav = line.split("\"")[1]
+				if len(line.split("\"")[1]) > 0:
+					tempchar.WavPath = pathlib.Path(aquanox_basepath, line.split("\"")[1])
+			if "Mood = " in line:
+				tempchar.Mood = line.split(" ")[2]
+			if line == "}":
+				listobj.table[entryname] = tempchar
+				tempchar = Character()
+				entryname = ""
+				readstate = "SearchTableEntry"
+				continue
+
+	if entryname != "":
+		print("Current TableEntry was not closed")
+		exit()
+	if readstate != "TableClosed":
+		print("Search did not find closing bracket")
+		exit()
+	file_des.close()
+
+	Lines = file_loc.readlines()
+	readstate = "TableStart"
+	tempchar = Stake()#Object will not be used
+	entryname = ""
+	tempcomment = ""
+	for line in Lines:
+		line = line.lstrip()
+		line = line.rstrip()
+		if len(line) == 0: #Empty Line
+			continue
+		if readstate == "TableStart":#Search for [Table]
+			if "[Table]" in line:
+				readstate = "TableOpen"
+				continue
+		if readstate == "TableOpen":#Search for { after [Table]
+			if line == "{":
+				readstate = "SearchTableEntry"
+				continue
+		if readstate == "SearchTableEntry":#Search for [
+			if line[0] == "[":
+				line = line.split("[")[1]
+				line = line.split("]")[0]
+				entryname = line
+				tempchar = listobj.table[entryname]
+				readstate = "TableEntryOpen"
+				continue
+		if readstate == "SearchTableEntry":#Search for last }
+			if line[0] == "}":
+				readstate = "TableClosed"
+				break#End
+		if readstate == "TableEntryOpen":
+			if line == "{":
+				readstate = "TableEntryData"
+				continue
+		if readstate == "TableEntryData":
+			if "//" in line:
+				if tempchar.Key == -1:
+					tempcomment = line.split("/")[2]
+				else:
+					tempchar.Comment_loc = line.split("/")[2] #Needed if table number is not stable
+			if "Key = " in line:
+				key = line.split(" ")[2]
+				if key != tempchar.Key:
+					print("Key in Tablekey in .loc does not match Key in Tablekey in .des")
+					print(tempchar)
+					print(key)
+					exit()
+			if "Text = " in line:
+				tempchar.Text = line.split("\"")[1]
+			if line == "}":
+				#listobj.table[entryname] = tempchar # Not needed as the object is already in the list
+				tempchar = Character()
+				entryname = ""
+				tempcomment = ""
+				readstate = "SearchTableEntry"
+				continue
+	file_loc.close()
+
 class Mood_List:
 	filepath_des = pathlib.Path(aquanox_basepath, "dat/sty/mood.des")
 	table = {}
@@ -541,3 +741,8 @@ shiplist.Export()
 moodlist = Mood_List()
 ParseMoods(moodlist)
 moodlist.Export()
+
+stakelist = Stake_List()
+ParseStakes(stakelist)
+print(stakelist.table["Take3"].Mood)
+stakelist.Export()
